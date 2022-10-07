9d4d49ce0f3f34c73e52fc77ca73c7b95d06de4c
Anatoly <slabodenyukanatoly@gmail.com>
2022-10-07 11:03:41 +0700

    Add create SQL database

---
+++ Работы/SQL/Практическая работа/Курс от Stepik.sql
@@ -0,0 +1,13 @@
+# Практическая работа по курсу "Интерактивный тренажер по SQL" от Stepik
+
+# Создание БД
+CREATE DATABASE qa_junior;
+
+# Создание таблицы
+CREATE TABLE skills (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+    hardskills VARCHAR(30),
+    softskills VARCHAR(30),
+    tools VARCHAR(30)
+);
+
